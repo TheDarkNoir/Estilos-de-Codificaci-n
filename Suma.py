f9fff51ddc1f37d1bdbd11bbd12886dd095f0d86
Antoine <antoinevislan@gmail.com>
2026-03-18 05:31:55 +0000

    Agregar archivos de funciones matemáticas: suma, resta, multiplicación y división

---
+++ Suma.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    """Retorna la suma de dos números.
+
+    Parámetros:
+    - a (int|float): primer sumando
+    - b (int|float): segundo sumando
+    Retorna:
+    - int|float: resultado de a + b
+    """
+    return a + b
